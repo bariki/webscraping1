a49f5605db24ee7786d49e1bab4e3a5e059fae93
Bariki Elilaki <bariki.elilaki@gmail.com>
2019-10-14 23:50:07 -0400

    final changes

--- gsmarena/analysis/main.py
+++ gsmarena/analysis/main.py
@@ -2,7 +2,6 @@ import mysql.connector
 import pandas as pd
 import matplotlib.pyplot as plt
 
-dbCon = mysql.connector.connect( host="localhost", user="root", passwd="", database="python1" )
-df = pd.read_sql('SELECT * FROM table_name', con=dbCon)
+#Please open file visulization with jupiter note book
 
 
